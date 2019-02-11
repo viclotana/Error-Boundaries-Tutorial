fca106f53b1fa02a29b6ea2960a64d831a9a0ac6
Nwose Lotanna Victor <viclotana@gmail.com>
2019-02-11 01:35:21 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Artists from './Artists';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+      <h4>Artists Performing at Tonight's Grammy</h4>
+       <Artists artistName='Davido'></Artists>
+       <Artists artistName='Mayorkun'></Artists>
+       <Artists artistName='Dremo'></Artists>
+       <Artists artistName='Sinzu'></Artists>
       </div>
     );
   }
