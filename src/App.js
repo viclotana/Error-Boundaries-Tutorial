802361a9e9de862fd08bbf054bb5555a1531d980
mac <mac@macs-MacBook-Pro.local>
2019-02-11 01:38:29 +0100

    hhh

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+<<<<<<< HEAD
       <h4>Artists Performing at Tonight's Grammy</h4>
        <Artists artistName='Davido'></Artists>
        <Artists artistName='Mayorkun'></Artists>
        <Artists artistName='Dremo'></Artists>
        <Artists artistName='Sinzu'></Artists>
+=======
+
+>>>>>>> 80bc6fcc7ed3a83448d7945a1a5aa869982818c0
       </div>
     );
   }
