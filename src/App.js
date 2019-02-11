189d9710e80c3a5635c81ee334e505befaaa3c27
mac <mac@macs-MacBook-Pro.local>
2019-02-11 08:14:16 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
 import Artists from './Artists';
+import ErrorBoundary from './ErrorBoundary'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <h4>Artists Performing at Tonight's Grammy</h4>
-       <Artists artistName='Davido'></Artists>
-       <Artists artistName='Mayorkun'></Artists>
-       <Artists artistName='Dremo'></Artists>
-       <Artists artistName='peruzzi'></Artists>
+      <ErrorBoundary>
+        <Artists artistName='Davido'></Artists>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Artists artistName='Dremo'></Artists>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Artists artistName='peruzzi'></Artists>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Artists artistName='Mayorkun'></Artists>
+      </ErrorBoundary>
+      
       </div>
     );
   }
